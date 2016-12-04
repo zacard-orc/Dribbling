9aafe6a50bcd7abfe501a982f6bcb50333f6cf84
13816025562@139.com <13816025562@139.com>
2016-12-04 23:04:36 +0800

    0.0.3:Aspree Build

--- Aspree/assets/za.js
+++ Aspree/assets/za.js
@@ -2,11 +2,31 @@
  * Created by z30 on 16/12/3.
  */
 
-
+//Normal Js
 window.onload=function(){
   document.getElementById("z_info").style.height=document.body.scrollHeight+'px';
-
   document.getElementById("suntext").style.width=document.getElementById("sun1").offsetWidth*2+'px';
   document.getElementById("suntext").style.height=document.getElementById("sun1").offsetHeight+'px';
+  //console.log(document.getElementById("vdmenu_1").style);
+  //console.log(document.getElementById("vdmenu_1").style);
 
 };
+
+//Vue
+new Vue({
+  el: '#vdmenu_1',
+  data: {
+    show: false
+  },
+  methods:{
+    showlv2menu:function(e){
+      this.$el.children[1].style.display='flex';
+    },
+    closelv2menu:function(e){
+      this.$el.children[1].style.display='none';
+    }
+  },
+  mounted:function(){
+    this.$el.children[1].style.left=this.$el.offsetWidth+'px';
+  }
+});
