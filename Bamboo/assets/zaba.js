0e93d57bcfef765b19a840a1b92543a3144e11e5
13816025562@139.com <13816025562@139.com>
2017-01-13 15:41:20 +0800

    0.0.6:Bamboo Building

---
+++ Bamboo/assets/zaba.js
@@ -0,0 +1,24 @@
+/**
+ * Created by z30 on 17/1/13.
+ */
+
+
+//Normal Js
+window.onload=function(){
+  document.getElementById("z_container").style.height=document.body.scrollHeight+'px';
+  //document.getElementById("suntext").style.width=document.getElementById("sun1").offsetWidth*2+'px';
+  //document.getElementById("suntext").style.height=document.getElementById("sun1").offsetHeight+'px';
+  //
+  //document.getElementById("wther_filter").style.width=document.getElementById("wther_bg").offsetWidth+'px';
+  //document.getElementById("wther_filter").style.height=document.getElementById("wther_bg").offsetHeight+'px';
+  //
+  //document.getElementById("wther_main").style.width=document.getElementById("wther_filter").offsetWidth+'px';
+  //document.getElementById("wther_main").style.height=document.getElementById("wther_filter").offsetHeight+'px';
+  //
+  //document.getElementById("wther_chart").style.width=document.getElementById("wther_filter").offsetWidth+'px';
+  //document.getElementById("wther_chart").style.height=document.getElementById("wther_filter").offsetHeight+'px';
+
+  //console.log(document.getElementById("vdmenu_1").style);
+  //console.log(document.getElementById("vdmenu_1").style);
+
+};
