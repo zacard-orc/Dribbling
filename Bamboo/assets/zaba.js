8d6858a0c74a863c65af3287ae0573f17c9f5a03
13816025562@139.com <13816025562@139.com>
2017-01-14 16:09:48 +0800

    0.0.6:Bamboo Building

--- Bamboo/assets/zaba.js
+++ Bamboo/assets/zaba.js
@@ -16,11 +16,19 @@ $("[id^='b_sm']").slider({
   }
 });
 
-////$(".za-bfun4-select").select2();
-//$("#arhv").hover(function(){
-//  //鼠标进入
-//  $(this).addClass('za-anm-hoverIn').css('left','0');
-//},function(){
-////  鼠标离开
-//});
-//arhv_close
+//$(".za-bfun4-select").select2();
+$("#arhv").hover(function(){
+  //鼠标进入
+  $(this)
+    .removeClass('za-anm-hoverOut')
+    .addClass('za-anm-hoverIn')
+    .css('left','0');
+},function(){
+//  鼠标离开
+});
+$("#arhv_close").click(function(){
+  $("#arhv")
+    .removeClass('za-anm-hoverIn')
+    .addClass('za-anm-hoverOut')
+    .css('left','-270px');
+});
